86606834b28268dbbc4a3f82abd85667b5edcdea
urmala99 <53155350+urmala99@users.noreply.github.com>
2022-04-19 04:15:39 +0300

    bmi-laskuri, prof, home

---
+++ app/src/main/java/com/example/fitness_application/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fitness_application
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitness_application.databinding.ActivityBmiBinding
+import com.example.fitness_application.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bmi.setOnClickListener{
+            val intent = Intent(this, BMIActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
