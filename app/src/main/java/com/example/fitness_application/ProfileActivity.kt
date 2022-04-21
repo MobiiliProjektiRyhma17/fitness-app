215a29251f76c1e971d2335a7b85f6d98a9267ed
urmala99 <53155350+urmala99@users.noreply.github.com>
2022-04-21 23:16:28 +0300

    prof, adding background

--- app/src/main/java/com/example/fitness_application/ProfileActivity.kt
+++ app/src/main/java/com/example/fitness_application/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.fitness_application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.ActionBar
 import com.example.fitness_application.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,6 +12,13 @@ import com.google.firebase.database.FirebaseDatabase
 
 class ProfileActivity : AppCompatActivity() {
 
+
+    private lateinit var nameEdit: EditText
+    private lateinit var addressEdit: EditText
+    private lateinit var phoneNumberEdit: EditText
+    private lateinit  var goalsMenu: AutoCompleteTextView
+    private lateinit  var shapesMenu: AutoCompleteTextView
+    private lateinit var editButton: Button
 
 
 
@@ -23,6 +31,7 @@ class ProfileActivity : AppCompatActivity() {
 
     //Firebase
     private lateinit var firebaseAuth: FirebaseAuth
+
     private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +43,15 @@ class ProfileActivity : AppCompatActivity() {
         actionBar.title = "Profiili"
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Profiles")
+
+        nameEdit = findViewById(R.id.nameEdit)
+        addressEdit = findViewById(R.id.addressEdit)
+        phoneNumberEdit = findViewById(R.id.phoneNumberEdit)
+        editButton = findViewById(R.id.editButton)
+
+        editButton.setOnClickListener {
+            saveProduct()
+        }
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
@@ -47,6 +65,45 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
+
+        val goals = resources.getStringArray(R.array.goals)
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdown_item,goals)
+        val shapes = resources.getStringArray(R.array.shapes)
+        val arrayAdapter2 = ArrayAdapter(this,R.layout.dropdown_item,shapes)
+        goalsMenu = findViewById<AutoCompleteTextView>(R.id.autoGoal)
+        shapesMenu = findViewById<AutoCompleteTextView>(R.id.shapeSelect)
+        goalsMenu.setAdapter(arrayAdapter)
+        shapesMenu.setAdapter(arrayAdapter2)
+
+    }
+
+    class Profile(val ID: String, val Nimi: String, val Osoite: String, val Puhelinnumero: String, val Tavoite: String, val Kuntotaso: String){
+
+        constructor() : this("","","","", "", ""){
+
+        }
+    }
+
+    private fun saveProduct() {
+        val name = nameEdit.text.toString().trim()
+        val address = addressEdit.text.toString().trim()
+        val number =  phoneNumberEdit.text.toString().trim()
+        val goal = goalsMenu.text.toString().trim()
+        val shape = shapesMenu.text.toString().trim()
+
+        if(name.isEmpty()){
+            nameEdit.error = "Ole hyvä ja syötä nimesi"
+            return
+        }
+
+        val profileID: String? = databaseReference.push().key
+
+        val info = Profile(profileID.toString(), name, address, number, goal, shape)
+
+        databaseReference.child(profileID.toString()).setValue(info).addOnCompleteListener {
+            Toast.makeText(applicationContext, "Tiedot päivitetty onnistuneesti", Toast.LENGTH_LONG).show()
+        }
+
     }
 
     private fun checkUser() {
@@ -58,5 +115,6 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+
     }
 }
